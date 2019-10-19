2d2585c231ca25af4791f8270e86b1d72ca0b2d6
ygg <scyggdrasil@outlook.com>
2019-10-19 13:18:29 -0400

    extend touch

--- crossyuu-zing.js
+++ crossyuu-zing.js
@@ -1,6 +1,6 @@
 var touchArea = new ZingTouch.Region(document.getElementById('touch-events'));
 
-var wholeScreen = document.getElementById("crossyuu-canvas");
+var wholeScreen = document.getElementById("touch-events");
 
 touchArea.bind(wholeScreen, 'tap', (e) => {
   keyboardKeyPress(global.keymap.up);
@@ -9,7 +9,6 @@ touchArea.bind(wholeScreen, 'tap', (e) => {
 touchArea.bind(wholeScreen, 'swipe', (e) => {
   var dir = e.detail.data[0].currentDirection;
   dir += 45; // adjust direction
-  console.log(dir)
   if (dir < 90 || dir > 360) {
     keyboardKeyPress(global.keymap.right)
   } else if (dir < 180) {
